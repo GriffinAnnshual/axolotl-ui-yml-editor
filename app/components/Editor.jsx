f7342e55a433e4f89a2600a91cd0876f72ff2ae5
Griffin Annshual <griffintbr@gmail.com>
2024-03-17 13:33:28 +0530

    fixes done

--- app/components/Editor.jsx
+++ app/components/Editor.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import YamlEditor from "@focus-reactive/react-yaml"
 import {useRef, useEffect} from 'react'
 function Editor({ jsonContent, isLoaded }) {
+	const handleChange = ({ json, text }) => {
+		// console.log(json)
+		console.log("hi")
+	}
 	if (isLoaded) {
 		return (
-			<div className="w-[45%] mt-[100px]  pb-[3rem] h-[100vh] overflow-auto rounded-xl m-12  fixed bg-white border-2 border-black  ">
+			<div className="w-[45%] mx-auto mt-[100px] pb-[3rem] h-[100vh] overflow-auto rounded-xl m-12  fixed bg-white border-2 border-black  ">
 				<YamlEditor
+					onChange={handleChange}
 					text="# Yaml code editor
 					 Please Upload Your File to Edit"
 				/>
 			</div>
 		)
 	}
-
 	const actions = useRef(null)
 	useEffect(() => {
 		// Here we have access to imperative actions
@@ -23,6 +27,7 @@ function Editor({ jsonContent, isLoaded }) {
 	return (
 		<div className="w-[45%] mx-auto mt-[100px] pb-[3rem] h-[100vh] overflow-auto rounded-xl m-12  fixed bg-white border-2 border-black  ">
 			<YamlEditor
+				onChange={handleChange}
 				ref={actions}
 				json={jsonContent}
 			/>
